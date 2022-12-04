c8f0d1283f58478ca3c2e4b8ba418e8594ec832a
ramasubbaiah <ramasubbaiah@LAPTOP-8FTKGC45>
2022-12-04 12:39:41 +0530

    MAVEN PROJECT

---
+++ src/test/java/DEVOPSLAB/MH1A0579/AppTest.java
@@ -0,0 +1,20 @@
+package DEVOPSLAB.MH1A0579;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
